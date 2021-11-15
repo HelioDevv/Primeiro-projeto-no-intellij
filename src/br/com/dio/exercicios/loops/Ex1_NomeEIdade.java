06ffc870e2f9a08903660f56c7d90a4b355bef53
HÉLIO AUGUSTO NUNES JUNIOR <helio.guitar.virtu@gmail.com>
2021-11-15 16:54:44 -0300

    first commit direct intellij IDE

---
+++ src/br/com/dio/exercicios/loops/Ex1_NomeEIdade.java
@@ -0,0 +1,21 @@
+package br.com.dio.exercicios.loops;
+
+import java.util.Scanner;
+
+public class Ex1_NomeEIdade {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String nome;
+        int idade;
+
+        while(true){
+            System.out.println("Diga o Nome: ");
+            nome = scan.next();
+            if(nome.equals("0")) break;
+            System.out.println("Diga a Idade: ");
+            idade = scan.nextInt();
+        }
+        System.out.println("Continua aqui...");
+    }
+}
